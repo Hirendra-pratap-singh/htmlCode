d84cddfc3f4eac64e1437e0fae71669bb164a101
Hirendra pratap singh <hirendrapratapsingh981@gmail.com>
2024-07-07 13:00:48 +0530

    hirexhtmlCode

---
+++ app.py
@@ -0,0 +1,39 @@
+
+# start = 1  # Replace with your desired start value
+# end = 10   # Replace with your desired end value
+
+# for number in range(start, end):
+#     print(number)
+
+# listno=[1,2,3,4,5,6]
+
+# for no in listno:
+
+#     print(no)
+# listno=[1,2,3,4,5,6,7,8,9,10]
+
+# for i in listno:
+
+#     print(i)
+
+# def say_hello():
+#     print("hello,world")
+
+#     say_hello()
+
+# def print_sum(num1, num2):
+#     result = num1 + num2
+#     print("Sum:", result)
+
+# # Call the function with two numbers as arguments
+# # num1 = 5
+# # num2 = 7
+# print_sum(1,2)
+
+def print_sum(no1,no2):
+
+    sum=no1+no2
+
+    print("result:",sum)
+
+    print_sum(3,7)
